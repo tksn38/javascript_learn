a0922d225275f15addaab1442391a424636a0b96
tksn38 <tksn38@pekoro.moe>
2026-01-02 00:14:54 +0900

    add(test01.js/test02.js in 202601020014

---
+++ test01.js
@@ -0,0 +1,19 @@
+//字面量
+
+//a = 1
+var a = 1
+var b = 2
+// function myFunction(a,b) {
+//     return a * b;
+// }
+// console.log(myFunction);
+
+var length = 16;
+//var lastname = "Jhonson";
+var cars = ["Toyota", "Honda", "Suzuki"];
+//console.log(length+lastname, cars);
+
+var person = {firstname:"John", lastname:"Doe"};
+//console.log(person,length,cars);
+console.log(16 + "Volvo");
+
